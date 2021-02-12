3cba3e00b83a42ebbfa499f4a46a84550c42bda6
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-12 15:59:16 -0600

    Initial Commit

---
+++ good-news-app/good_news_appApp.swift
@@ -0,0 +1,20 @@
+//
+//  good_news_appApp.swift
+//  good-news-app
+//
+//  Created by Arpan Dhatt on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct good_news_appApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
