e491083c698bfd5bbdf16f582484bd6a8766dc0b
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-12 17:34:55 -0600

    added profile view

--- good-news-app/FeedView.swift
+++ good-news-app/FeedView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State private var showProfileView = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationBarTitle("Feed").navigationBarItems(trailing: Button(action: {
+                showProfileView.toggle()
+            }) {
+                Label("Profile", systemImage: "person.circle.fill")
+            })
+        }.sheet(isPresented: $showProfileView) {
+            ProfileView()
+        }
     }
 }
 
