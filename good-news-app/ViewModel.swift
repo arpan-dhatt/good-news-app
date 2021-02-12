8eda76c83d64c373ff0f3dccf5f8bbd253be25fa
SaakethC <saakethrchenna@gmail.com>
2021-02-12 17:50:56 -0600

    tried to fix userdeafaults

--- good-news-app/ViewModel.swift
+++ good-news-app/ViewModel.swift
@@ -14,13 +14,18 @@ class ViewModel: ObservableObject {
     @Published var onboarding: Bool
     @Published var user: InfoModel.User {
         didSet{
-            UserDefaults.standard.set(user, forKey: "user")
+            //UserDefaults.standard.set(user, forKey: "user")
+            UserDefaults.standard.set(user.name, forKey: "name")
+            UserDefaults.standard.set(user.sources, forKey: "sources")
         }
     }
     
     //User Defaults
     init() {
         self.onboarding = false
-        self.user = UserDefaults.standard.object(forKey: "user") as? InfoModel.User ?? InfoModel.User(name: "Arpan", sources: ["yes"], categories: ["one", "two"], suggestions: ["three", "four"])
+        //self.user = UserDefaults.standard.object(forKey: "user") as? InfoModel.User ??
+        self.user = InfoModel.User(name: "Arpan", sources: ["yes"], categories: ["one", "two"], suggestions: ["three", "four"])
+        self.user.name = UserDefaults.standard.object(forKey: "name") as? String ?? "Arpan"
+        self.user.sources = UserDefaults.standard.object(forKey: "sources") as? [String] ?? ["heelo"]
     }
 }
