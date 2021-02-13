2832964f33878bb8f55fb6ef6a42079dcfe44d78
SaakethC <saakethrchenna@gmail.com>
2021-02-12 20:26:59 -0600

    made safariview

--- good-news-app/FeedList.swift
+++ good-news-app/FeedList.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct FeedList: View {
     @StateObject var dataSource = FeedDataSource()
+    @State var presentingSafariView = false
+    @State var currentURL = ""
     
     var body: some View {
         ScrollView {
@@ -16,7 +19,12 @@ struct FeedList: View {
                 ForEach(dataSource.items, id: \.self) { item in
                     BasicNewsCard(title: item.title, subtitle: item.subtitle, article: item.article, date: item.date, description: item.description, thumbnail: item.thumbnail, categories: item.categories).onAppear {
                         dataSource.loadMoreContentIfNeeded(currentItem: item)
+                    }.onTapGesture {
+                        self.presentingSafariView = true
+                        self.currentURL = item.article
                     }
+                }.sheet(isPresented: $presentingSafariView){
+                    SafariView(url: URL(string: currentURL)!)
                 }
                 if dataSource.isLoadingPage {
                     ProgressView()
@@ -24,7 +32,19 @@ struct FeedList: View {
             }
         }
     }
+    
 }
+    
+    struct SafariView: UIViewControllerRepresentable{
+        var url: URL
+        func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+            let safariView = SFSafariViewController(url: url, entersReaderIfAvailable: true)
+            return safariView
+        }
+        func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        }
+        
+    }
 
 struct FeedList_Previews: PreviewProvider {
     static var previews: some View {
