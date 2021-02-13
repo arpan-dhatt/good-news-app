7b1661a1cb74e2648eda5472bdf3aa2e6a31fd91
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-13 10:27:29 -0600

    fixed url construction

--- good-news-app/FeedDataSource.swift
+++ good-news-app/FeedDataSource.swift
@@ -80,10 +80,14 @@ class FeedDataSource: ObservableObject {
         isLoadingPage = true
         var url_obj: URL? = nil;
         if orderType == OrderType.feed {
-            url_obj = URL(string: "http://66.169.166.210:8080/recommendations?sources=\(user.sources.joined(separator: ","))&categories=\(user.categories.joined(separator: ","))&suggested=\(user.suggestions.joined(separator: ","))&page=\(currentPage)")
+            url_obj = URL(string: "http://66.169.166.210:8080/recommendations?sources=\(user.sources.joined(separator: ",").filter {c in c != " "})&categories=\(user.categories.joined(separator: ",").filter {c in c != " "})&suggested=\(user.suggestions.joined(separator: ",").filter {c in c != " "})&page=\(currentPage)")
+//            print("http://66.169.166.210:8080/recommendations?sources=\(user.sources.joined(separator: ",").filter {c in c != " "})&categories=\(user.categories.joined(separator: ",").filter {c in c != " "})&suggested=\(user.suggestions.joined(separator: ",").filter {c in c != " "})&page=\(currentPage)")
+//            url_obj = URL(string: "http://66.169.166.210:8080/categorical?sources=n,n&category=n,n&page=\(currentPage)")
         }
         else {
-            url_obj = URL(string: "http://66.169.166.210:8080/categorical?sources=\(user.sources.joined(separator: ","))&category=\(user.categories.joined(separator: ","))&page=\(currentPage)")
+            url_obj = URL(string: "http://66.169.166.210:8080/categorical?sources=\(user.sources.joined(separator: ",").filter {c in c != " "})&category=\(user.categories.joined(separator: ",").filter {c in c != " "})&page=\(currentPage)")
+            print(url_obj?.absoluteURL)
+//            url_obj = URL(string: "http://66.169.166.210:8080/categorical?sources=n,n&category=n,n&page=\(currentPage)")
         }
         guard let url = url_obj else { return }
         URLSession.shared.dataTaskPublisher(for: url)
